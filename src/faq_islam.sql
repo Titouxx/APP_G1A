dfb715b41447a1cbaa71ca91b3dbb9010397bc7b
Islem618 <isas61270@eleve.isep.fr>
2023-12-03 15:04:25 +0100

    Islam_placeholder_
    début de code pour le script sql, les scripts php js et html permettant l'envoi et la réception de mail, mais impossible a executer sans la base de donnée contenant les infos user et admin

--- src/faq_islam.sql
+++ src/faq_islam.sql
@@ -1,9 +1,39 @@
 ﻿CREATE TABLE liste_faq (
-    ID INT AUTO_INCREMENT PRIMARY KEY,
-    Prenom nvarchar(255),
-    NomDeFamille nvarchar(255),
-    email nvarchar(320),
+    ID INT PRIMARY KEY IDENTITY(1,1),
+    Prenom NVARCHAR(255),
+    NomDeFamille NVARCHAR(255),
+    email NVARCHAR(320),
     utilisateur_id INT,
-    FOREIGN KEY (utilisateur_id) REFERENCES Utilisateurs(ID),
-    );
-    
+    FOREIGN KEY (utilisateur_id) REFERENCES Utilisateurs(ID)
+);
+
+USE faq_islam;
+
+DECLARE @EmailUtilisateur NVARCHAR(320);
+DECLARE @Sujet NVARCHAR(255) = 'Sujet de l''email';
+DECLARE @CorpsMessage NVARCHAR(MAX) = 'Contenu de votre message ici.';
+
+-- Récupérer l'email de l'utilisateur en fonction de l'ID (par exemple, ID = 1)
+SELECT @EmailUtilisateur = email
+FROM Utilisateurs
+WHERE ID = 1; -- Remplacer 1 par l'ID de l'admin qui doit recevoir le mail afin de pouvoir y répondre
+
+-- Vérifier si l'email de l'utilisateur existe
+IF @EmailUtilisateur IS NOT NULL
+BEGIN
+    -- Envoi de l'email
+    EXEC msdb.dbo.sp_send_dbmail
+        @profile_name = 'NomProfilEmail', -- Remplacez 'NomProfilEmail' par le nom de votre profil Email - Admin
+        @recipients = @EmailUtilisateur,
+        @subject = @Sujet,
+        @body = @CorpsMessage;
+        
+    PRINT 'Email envoyé avec succès à l''utilisateur.';
+END
+ELSE
+BEGIN
+    PRINT 'L''email de l''utilisateur n''a pas été trouvé.';
+END
+
+-- Execution de l'envoi du mail
+EXEC EnvoiEmailUtilisateur @utilisateur_id = 1; -- Remplacer 1 par l'ID de l'admin qui doit recevoir le mail afin de pouvoir y répondre
