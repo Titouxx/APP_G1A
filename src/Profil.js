85abbcafaabbd3a54879a97328f5ca26303bc3f8
OussamaBedadi <152306248+OussamaBedadi@users.noreply.github.com>
2023-12-18 20:18:47 +0100

    sans barre de menu mais modification et enregistrement de données

--- src/Profil.js
+++ src/Profil.js
@@ -8,3 +8,27 @@ function showSection(sectionId) {
     }
   });
 }
+
+function activerEdition() {
+  var champsEditable = document.querySelectorAll(
+    "#coordonnees input[readonly]"
+  );
+  champsEditable.forEach(function (champ) {
+    champ.removeAttribute("readonly");
+  });
+
+  // Afficher le bouton "Enregistrer" et masquer le bouton "Modifier"
+  document.getElementById("Enregistrer").style.display = "inline-block";
+  document.getElementById("Modifier").style.display = "none";
+}
+
+function enregistrerEdition() {
+  var champsEditable = document.querySelectorAll("#coordonnees input");
+  champsEditable.forEach(function (champ) {
+    champ.setAttribute("readonly", "true");
+  });
+
+  // Afficher le bouton "Modifier" et masquer le bouton "Enregistrer"
+  document.getElementById("Modifier").style.display = "inline-block";
+  document.getElementById("Enregistrer").style.display = "none";
+}
