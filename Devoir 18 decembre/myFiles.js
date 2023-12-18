bafcaafc2d8ed850a928cc2fb42b60157d9f12cb
OussamaBedadi <152306248+OussamaBedadi@users.noreply.github.com>
2023-12-18 14:29:05 +0100

    Devoir APP JS

---
+++ Devoir 18 decembre/myFiles.js
@@ -0,0 +1,36 @@
+function fonction1() {
+  document.write("Hello World!");
+  document.write("<h1>Hello World!</h1>");
+}
+
+function fonction2() {
+  let x = 5 + 5;
+  document.write(x);
+  document.write("<br/>");
+  x = "5" + "5";
+  document.write(x + "<br/>");
+  x = 5 + 1 + "5";
+  document.write(x);
+  document.write("<br/>");
+  x = "5" + 5 + 1;
+  document.write(x + "<br/>");
+}
+function fonction() {
+  var animals = new Array();
+  animals[0] = "Dog";
+  animals[1] = "Cat";
+  animals[2] = "Delphin";
+  for (a in animals) {
+    document.write(animals[a] + "<br />");
+  }
+}
+
+function produit(a, b) {
+  return a * b;
+}
+
+function prompt1() {
+  var a = prompt("Entrer la valeur de a");
+  var b = prompt("Entrer la valeur de b");
+  alert(a * b);
+}
