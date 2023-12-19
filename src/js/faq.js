7958ba49d65452568229b8d715964de496e29668
Islem618 <isas61270@eleve.isep.fr>
2023-12-19 21:32:38 +0100

    ajustements questions génériques
    ajustements css permettant que lorsqu'on clique sur une question, le "+" se transforme en "-" et inversement. Jusqu'a présent lorsque l'on cliquait sur une question le "+" restait inchangé. Un simple détail qui nécéssite pas mal de travail

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,22 +1,30 @@
-﻿
-const items = document.querySelectorAll(".accordion button"); //js questions génériques
+﻿// Sélection de tous les éléments .accordion button
+const items = document.querySelectorAll(".accordion button");
 
+// Fonction pour basculer l'état d'expansion de l'accordéon
 function toggleAccordion() {
+  // Récupère la valeur de l'attribut aria-expanded de l'élément actuel
   const itemToggle = this.getAttribute('aria-expanded');
-  
-  
-  
-  
-  for (i = 0; i < items.length; i++) {
-    items[i].setAttribute('aria-expanded', 'false');
-  }
-  
-  if (itemToggle == 'false') {
+
+  // Réinitialise tous les éléments à aria-expanded='false'
+  items.forEach(item => {
+    item.setAttribute('aria-expanded', 'false');
+  });
+
+  // Si l'élément actuel est fermé ou n'a pas d'état défini, le marque comme ouvert, sinon le ferme
+  if (itemToggle === 'false' || itemToggle === null) {
     this.setAttribute('aria-expanded', 'true');
+  } else {
+    this.setAttribute('aria-expanded', 'false');
   }
 }
 
+// Ajoute un écouteur d'événement pour chaque élément, déclenchant la fonction toggleAccordion lors d'un clic
 items.forEach(item => item.addEventListener('click', toggleAccordion));
+
+
+
+
 
 const arrowIcon = document.getElementById('hoverImage');// Hover pour l'image
 
@@ -33,7 +41,7 @@ arrowIcon.addEventListener('mouseout', function() {
 /* Barre de recherche type CTRL+F */
 document.getElementById('searchInput').addEventListener('input', function() {
   var searchValue = this.value.toLowerCase();
-  var elementsToSearch = document.querySelectorAll('.menu a, footer a, .accordion-title, p, h1, h2, h3, h4, h5, h6, #searchQuerySubmit');//Tout ce qui est sélectionné par le code
+  var elementsToSearch = document.querySelectorAll('.menu a, footer a, .accordion-title, p, h1, h2, h3, h4, h5, h6');//Tout ce qui est sélectionné par le code
 
   elementsToSearch.forEach(function(element) {
     var elementText = element.textContent.toLowerCase();
