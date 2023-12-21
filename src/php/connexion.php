74677f90b8128312354abf3c6f8a044f8606025c
Kelyan Taverny <taverny.k@gmail.com>
2023-12-21 19:48:27 +0100

    UPDATE CONNEXION + BDD + TRAITEMENT DES ERREURS

--- src/php/connexion.php
+++ src/php/connexion.php
@@ -102,15 +102,49 @@
             xhr.open("POST", "login.php", true);
             xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
             xhr.onload = function() {
-                var response = JSON.parse(this.responseText);
-                if (response.status === "success") {
-                    window.location.href = "../html/index.html"; // Redirection vers une nouvelle page
-                } else {
-                    alert(response.message); // Afficher le message d'erreur
+                console.log(this.responseText); // Afficher la réponse brute du serveur
+                try {
+
+                    var response = JSON.parse(this.responseText);
+                    console.log(response); // Afficher l'objet réponse
+                    if (response.status === "success") {
+                        window.location.href = "../html/index.html"; // Redirection vers une nouvelle page
+                    } else {
+                        // Afficher le message d'erreur
+                        alert(response.message || "Une erreur est survenue");
+                    }
+                } catch (e){
+                    console.error("Erreur de parsing JSON: ", e);
                 }
-            };
+            }   
             xhr.send("loginEmail=" + encodeURIComponent(email) + "&loginPassword=" + encodeURIComponent(password));
         });
+        document.getElementById("registerForm").addEventListener("submit", function(event) {
+            event.preventDefault();
+            var email = document.getElementById("registerEmail").value;
+            var password = document.getElementById("registerPassword").value;
+            var repeatPassword = document.getElementById("RepeatPassword").value;
+
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", "register.php", true);
+            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            xhr.onload = function() {
+                console.log(this.responseText); // Afficher la réponse brute du serveur
+                try {
+                    var response = JSON.parse(this.responseText);
+                    console.log(response); // Afficher l'objet réponse
+                    if (response.status === "success") {
+                        window.location.href = "../html/index.html"; // Redirection vers la même page que "login"
+                    } else {
+                        alert(response.message || "Une erreur est survenue lors de l'enregistrement");
+                    }
+                } catch (e) {
+                    console.error("Erreur de parsing JSON: ", e);
+                }
+            };
+            xhr.send("registerEmail=" + encodeURIComponent(email) + "&registerPassword=" + encodeURIComponent(password) + "&RepeatPassword=" + encodeURIComponent(repeatPassword));
+        });
+
     </script>
   </body>
 </html>
