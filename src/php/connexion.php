ee567c29f4503846146fd61ed232be50995b6c22
Islem618 <isas61270@eleve.isep.fr>
2024-01-13 23:42:00 +0100

    .

---
+++ src/php/connexion.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Connexion</title>
+    <link rel="stylesheet" href="../css/connexion.css" />
+    <link
+      href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
+      rel="stylesheet"
+    />
+  </head>
+  <body background="../../images/background.png">
+
+    
+    <!--Formulaire de connexion-->
+    <div class="wrapper" id="loginWrapper">
+      <form id="loginForm" action="login.php" method="post">
+        <h1>Login</h1>
+        <div class="input-box">
+          <input type="text" id="loginEmail" name="loginEmail" placeholder="Email" required /><i
+            class="bx bxs-user"
+          ></i>
+        </div>
+        <div class="input-box">
+          <input
+            type="password"
+            id="loginPassword"
+            name="loginPassword"
+            placeholder="Password"
+            required
+          /><i class="bx bxs-lock-alt"></i>
+        </div>
+
+        <div class="remember-forgot">
+          <label><input type="checkbox" /> Remember me</label>
+          <a href="#">Forgot password?</a>
+        </div>
+
+        <button type="submit" class="btn">Login</button>
+        <p>Don't have an account?</p>
+        <button type="button" id="registerBtn" class="btn">Register</button>
+      </form>
+    </div>
+
+
+    <div class="wrapper2" id="registerWrapper" style="display: none">
+      <form id="registerForm" action="register.php" method="post">
+        <h1>Register</h1>
+        <div class="input-box">
+          <input
+            type="text"
+            id="registerEmail"
+            name="registerEmail"
+            placeholder="Email"
+            required
+          /><i class="bx bxs-user"></i>
+        </div>
+        <div class="input-box">
+          <input
+            type="password"
+            id="registerPassword"
+            name="registerPassword"
+            placeholder="Password"
+            required
+          /><i class="bx bxs-lock-alt"></i>
+        </div>
+        <div class="input-box">
+          <input
+            type="password"
+            id="RepeatPassword"
+            name="RepeatPassword"
+            placeholder="Repeat Password"
+            required
+          /><i class="bx bxs-lock-alt"></i>
+        </div>
+
+        <button type="submit" class="btn">Register</button>
+        <p>Already have an account?</p>
+        <button type="button" id="loginBtn" class="btn">Sign in</button>
+      </form>
+    </div>
+     <script src = "../js/connexion.js"> </script>
+  </body>
+</html>
---
+++ src/php/index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width" />
+    <link href="../css/index.css" rel="stylesheet" />
+    <script src="../js/jquery.min.js"></script>
+    <link rel="icon" type="image/x-icon" href="../../images/TransNoise.ico" />
+    <title>TransNoise - EchoKey</title>
+  </head>
+
+  <!--header gere la barre du haut de l'écran-->
+
+  <header>
+    <nav>
+      <ul class="menu">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="analyse.php">Analyse</a></li>
+        <li><a href="faq.php">Faq</a></li>
+        <li><a href="espaceuser.php">Profil</a></li>
+      </ul>
+    </nav>
+  </header>
+  <!--body gere le corp de la page-->
+  <body>
+    <!--le logo en haut à gauche-->
+    <img
+      src="../../images/EchoKey_extrude.png"
+      id="Logo1"
+      alt="Logo EchoKey"
+      title="Logo EchoKey"
+    />
+
+    <!--gere le texte au milieu de la page-->
+    <div class="content">
+      <h1>
+        Trouvez
+        <br />
+        Votre <br />
+        Voix
+      </h1>
+      <img src="../../images/Image_index.jpeg" alt="chanteur" height="300px" />
+    </div>
+    <div class="content-container">
+      <div class="left">
+        <h1 class="Echokey">Echokey</h1>
+        <p class="paragraphe">
+          Beaucoup d'artistes ne disposent pas d'analyse précise de leurs
+          performances. Echokey propose une solution innovante, en plaçant des
+          capteurs dernier cri dans les plus grandes salles parisiennes tel que
+          l'Olympia, l'Opera Garnier ou encore le Bataclan. Ces données ainsi
+          collectées par nos capteurs sont analysées et un rapport graphique est
+          directement envoyé à nos clients.
+        </p>
+      </div>
+      <div class="right">
+        <h1>
+          Déjà plus de<br /><span>7 098 personnes </span><br />ont confiance
+          en notre expertise
+        </h1>
+      </div>
+    </div>
+    <div class="buton">
+      <button class="cn" id="scrollButton">
+        <a href="Connexion.php">Connectez Vous !</a>
+      </button>
+    </div>
+  </body>
+
+  <!--footer gere le bas de page-->
+  <footer>
+    <div class="footer">
+      <nav>
+        <ul>
+          <li><a href="CGU.php" target="_blank">C.G.U</a></li>
+          <li>
+            <a href="https://www.isep.fr/" target="_blank">Nos investisseurs</a>
+          </li>
+          <li><a href="faq.php" target="_blank">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
+  </footer>
+</html>
