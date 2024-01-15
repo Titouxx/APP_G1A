07d6c5c5096e0a1df20090baa530b57ccaa78ba2
ManelISEP <mnl25bnnsr@gmail.com>
2024-01-15 01:09:26 +0100

    update hover images

--- src/js/espaceuser.js
+++ src/js/espaceuser.js
@@ -27,3 +27,34 @@ document.addEventListener("click", (event) => {
 tab.addEventListener("click", (event) => {
   event.stopPropagation();
 });
+//popup servant a la deconnexion
+function deconnexion() {
+  var result = confirm("Voulez-vous vraiment vous déconnecter?");
+  if (result == true) {
+    alert("Merci de votre visite");
+    //+Insérer ici code permettant la déconnexion
+  } else {
+    //On ferme juste la popup puisqu'on ne se deconnecte pas en cliquant sur annuler
+  }
+}
+
+//JS Hover bouton déconnexion
+function changerImage(etat) {
+  var img = document.getElementById("imgdeco");
+  if (etat === "survol") {
+    img.src = "../../images/déconnexion-hover.png"; // Chemin vers l'image au survol
+  } else {
+    img.src = "../../images/déconnexion.png"; // Chemin vers l'image normale
+  }
+}
+
+var container = document.getElementById("image1");
+var text = document.getElementById("imageText");
+
+container.addEventListener("mouseover", function () {
+  text.style.display = "block";
+});
+
+container.addEventListener("mouseout", function () {
+  text.style.display = "none";
+});
