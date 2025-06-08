6496c7c256e7e343c0c3d946cc0502c342ef8cb0
Titouxx <titouan.bazillio.pro@gmail.com>
2025-06-08 08:00:05 +0200

    gitignore

--- FR/src/php/connectSQL.php
+++ FR/src/php/connectSQL.php
@@ -1,30 +1,9 @@
 <?php
-function getPDOConnection(): PDO {
-    $hostname = "localhost";
-    $base = "siteweb";
-    $loginBD = "root";
-    $passBD = ""; //root for mac
-
-    try {
-        $dsn = "mysql:host=$hostname;dbname=$base;charset=utf8";
-        $pdo = new PDO($dsn, $loginBD, $passBD, [
-            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-        ]);
-        return $pdo;
-    } catch (PDOException $e) {
-        echo "Erreur de connexion : " . $e->getMessage();
-        die();
-    }
-}
-
-
-// function getPDOConnection(): PDO
-// {
-//     $hostname = "mysql-nutritium.alwaysdata.net"; // hôte MySQL AlwaysData
-//     $base = "nutritium_db";                       // nom de la base
-//     $loginBD = "nutritium";                       // nom d'utilisateur MySQL
-//     $passBD = "Nutritiumadmin123*";
+// function getPDOConnection(): PDO {
+//     $hostname = "localhost";
+//     $base = "siteweb";
+//     $loginBD = "root";
+//     $passBD = ""; //root for mac
 
 //     try {
 //         $dsn = "mysql:host=$hostname;dbname=$base;charset=utf8";
@@ -40,4 +19,23 @@ function getPDOConnection(): PDO {
 // }
 
 
+function getPDOConnection(): PDO
+{
+    $hostname = "mysql-nutritium.alwaysdata.net"; // hôte MySQL AlwaysData
+    $base = "nutritium_db";                       // nom de la base
+    $loginBD = "nutritium";                       // nom d'utilisateur MySQL
+    $passBD = "Nutritiumadmin123*";
+
+    try {
+        $dsn = "mysql:host=$hostname;dbname=$base;charset=utf8";
+        $pdo = new PDO($dsn, $loginBD, $passBD, [
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]);
+        return $pdo;
+    } catch (PDOException $e) {
+        echo "Erreur de connexion : " . $e->getMessage();
+        die();
+    }
+}
 ?>
