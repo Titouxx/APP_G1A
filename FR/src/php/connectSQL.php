accc3a53c0182a262f00cc201f8b09cbe155f2b9
Titouxx <titouan.bazillio.pro@gmail.com>
2025-06-08 07:42:02 +0200

    Update connectSQL.php

--- FR/src/php/connectSQL.php
+++ FR/src/php/connectSQL.php
@@ -1,30 +1,9 @@
 <?php
-function getPDOConnection(): PDO {
-    $hostname = "localhost";
-    $base = "siteweb";
-    $loginBD = "root";
-    $passBD = ""; //root for mac
-
-    try {
-        $dsn = "mysql:host=$hostname;dbname=$base;charset=utf8";
-        $pdo = new PDO($dsn, $loginBD, $passBD, [
-            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-        ]);
-        return $pdo;
-    } catch (PDOException $e) {
-        echo "Erreur de connexion : " . $e->getMessage();
-        die();
-    }
-}
-
-
-// function getPDOConnection(): PDO
-// {
-//     $hostname = "mysql-nutritium.alwaysdata.net"; // hôte MySQL AlwaysData
-//     $base = "nutritium_db";                       // nom de la base
-//     $loginBD = "nutritium";                       // nom d'utilisateur MySQL
-//     $passBD = "Nutritiumadmin123*";
+// function getPDOConnection(): PDO {
+//     $hostname = "localhost";
+//     $base = "siteweb";
+//     $loginBD = "root";
+//     $passBD = ""; //root for mac
 
 //     try {
 //         $dsn = "mysql:host=$hostname;dbname=$base;charset=utf8";
@@ -38,6 +17,27 @@ function getPDOConnection(): PDO {
 //         die();
 //     }
 // }
+
+
+function getPDOConnection(): PDO
+{
+    $hostname = "mysql-nutritium.alwaysdata.net"; // hôte MySQL AlwaysData
+    $base = "nutritium_db";                       // nom de la base
+    $loginBD = "nutritium";                       // nom d'utilisateur MySQL
+    $passBD = "Nutritiumadmin123*";
+
+    try {
+        $dsn = "mysql:host=$hostname;dbname=$base;charset=utf8";
+        $pdo = new PDO($dsn, $loginBD, $passBD, [
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]);
+        return $pdo;
+    } catch (PDOException $e) {
+        echo "Erreur de connexion : " . $e->getMessage();
+        die();
+    }
+}
 
 
 ?>
